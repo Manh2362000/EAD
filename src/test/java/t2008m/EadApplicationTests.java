be89b7edf640901d389a91bbe9ce32c4cc27db03
mạnh giang <manh2362000@gmail.com>
2022-05-27 09:50:32 +0700

    Initial commit

---
+++ src/test/java/t2008m/EadApplicationTests.java
@@ -0,0 +1,13 @@
+package t2008m;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
